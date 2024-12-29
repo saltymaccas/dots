3777315fdd222a4e10fa54c54711bb50e4733ec4
freshfieldreds <freshfieldreds@gmail.com>
2024-12-30 09:23:32 +1100

    changes

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,48 +1,44 @@
 return {
-	{
-		"akinsho/bufferline.nvim",
-		opts = {
-			options = {
-				seperator_style = "padded_slant",
-				indicator = {
-					style = "underline",
-				},
-			},
-		},
-	},
-	{
-		"folke/noice.nvim",
-		enabled = false,
-	},
-	{
-		"nvimdev/dashboard-nvim",
-		enabled = false,
-	},
-	{ "3rd/image.nvim", enabled = false },
-	{
-		"3rd/diagram.nvim",
-		enabled = false,
-		dependencies = {
-			"3rd/image.nvim",
-		},
-		opts = { -- you can just pass {}, defaults below
-			renderer_options = {
-				mermaid = {
-					background = "transparent", -- nil | "transparent" | "white" | "#hex"
-					theme = "dark", -- nil | "default" | "dark" | "forest" | "neutral"
-					scale = 1, -- nil | 1 (default) | 2  | 3 | ...
-				},
-				plantuml = {
-					charset = nil,
-				},
-				d2 = {
-					theme_id = nil,
-					dark_theme_id = nil,
-					scale = nil,
-					layout = nil,
-					sketch = nil,
-				},
-			},
-		},
-	},
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        seperator_style = "padded_slant",
+        indicator = {
+          style = "underline",
+        },
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+  { "3rd/image.nvim", enabled = false },
+  {
+    "3rd/diagram.nvim",
+    enabled = false,
+    dependencies = {
+      "3rd/image.nvim",
+    },
+    opts = { -- you can just pass {}, defaults below
+      renderer_options = {
+        mermaid = {
+          background = "transparent", -- nil | "transparent" | "white" | "#hex"
+          theme = "dark", -- nil | "default" | "dark" | "forest" | "neutral"
+          scale = 1, -- nil | 1 (default) | 2  | 3 | ...
+        },
+        plantuml = {
+          charset = nil,
+        },
+        d2 = {
+          theme_id = nil,
+          dark_theme_id = nil,
+          scale = nil,
+          layout = nil,
+          sketch = nil,
+        },
+      },
+    },
+  },
 }
