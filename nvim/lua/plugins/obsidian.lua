08a9a78bb393de1b001cc4f57b0fcbf59c83cc4e
marcusnewton <newtonm472@gmail.com>
2025-08-28 19:34:14 +1000

    changes

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,29 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    -- refer to `:h file-pattern` for more examples
+    "BufReadPre "
+      .. vim.fn.expand("~")
+      .. "/notes/*.md",
+    "BufNewFile " .. vim.fn.expand("~") .. "/notes/*.md",
+  },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/notes",
+      },
+    },
+  },
+}
